cf196ed6526cbece6f3ebc669c971a9b8a879336
Larry Lee <llee454@gmail.com>
2020-03-27 14:18:43 -0700

    Improved the printing from compare_trace using the new pad function.

--- compare_trace.sh
+++ compare_trace.sh
@@ -210,6 +210,10 @@ function build_model {
       BEGIN {
         address_count = 0;
       }
+      function pad(string, len, z) {
+        z = "0000000000000000";
+        return substr(substr(z, length(z) - len) string, length(string) + 1)
+      }
       {
         if ($0 ~ /Mode:/)  {
           x["mode"] = $2;
@@ -228,7 +232,7 @@ function build_model {
           address_count ++;
         }
         if ($0 ~ /Inc PC/) {
-          output = "mode: " x["mode"] " pc: " x["pc"] " inst: " x["inst"] " reg write: " x["result"] " physical addresses:";
+          output = "mode: " x["mode"] " pc: " pad(x["pc"], 16) " inst: " pad(x["inst"], 8) " reg: " pad(x["result"], 16);
           for (i = 0; i < address_count; i ++) {
             output = output " " pad(x["addresses"][i], 16);
           }
